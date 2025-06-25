3ba17410c20c3b28064210d61908878e03a8cd20
hammad atta <64803448+Hammadatta@users.noreply.github.com>
2025-06-25 01:39:11 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/qsaf-framework-ghpages/',
-  plugins: [react()]
+  base: '', // ✅ Vercel requires this to be empty
+  plugins: [react()],
 });
