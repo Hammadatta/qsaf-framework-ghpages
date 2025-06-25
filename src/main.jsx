dadb8562d4c98c6b200a8ccc575209ceb169e7be
maadivan <hmmdatta@hotmail.com>
2025-06-25 01:12:42 -0700

    Updates

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import QSAFControls from './QSAFControls';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QSAFControls />
+  </React.StrictMode>
+);
